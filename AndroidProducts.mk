02ce30c6af877b5bee32a5b0baf292a2ee6286f2
Shirayuki39 <lorddemecrius83@proton.me>
2024-08-10 13:25:30 +0800

    LG8n: Fix COMMON_LUNCH_CHOICES
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/derp_LG8n.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_LG8n-user \
-    derp_LG8n-userdebug \
-    derp_LG8n-eng
+    derp_LG8n-ap2a-user \
+    derp_LG8n-ap2a-userdebug \
+    derp_LG8n-ap2a-eng
