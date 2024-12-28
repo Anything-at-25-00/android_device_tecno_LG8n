85a4b2207ee8486a686257f9809ca2608ca8c2fd
Shirayuki39 <lorddemecrius83@proton.me>
2024-12-28 13:28:14 +0800

    LG8n: fix miku flags
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- miku_LG8n.mk
+++ miku_LG8n.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/tecno/LG8n/device.mk)
 
 # Inherit some common Miku UI stuff.
-$(call inherit-product, vendor/miku/config/common_full_phone.mk)
+$(call inherit-product, vendor/miku/build/product/miku_product.mk)
 
 BOARD_VENDOR := TECNO
 PRODUCT_NAME := miku_LG8n
@@ -22,9 +22,6 @@ PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO LG8n
 
 PRODUCT_GMS_CLIENTID_BASE := android-transsion
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    DeviceName=LG8n
 
 # Maintaier
 MIKU_MASTER := Ren(Shirayuki39/Shirayuki428)
