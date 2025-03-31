32bb040a094e1572bcd0d71fd9098a856e264439
Shirayuki39 <lorddemecrius83@proton.me>
2025-03-31 16:05:04 +0800

    LG8n: Add more AxionAOSP Flags
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- lineage_LG8n.mk
+++ lineage_LG8n.mk
@@ -33,3 +33,30 @@ AXION_CAMERA_FRONT_INFO := 8
 AXION_MAINTAINER := Shirayuki39
 AXION_PROCESSOR := Helio_G99
 PRODUCT_NO_CAMERA := false
+BYPASS_CHARGE_SUPPORTED := false
+
+# Define small and big core groups
+AXION_CPU_SMALL_CORES := 0,1,2,3,4,5
+AXION_CPU_BIG_CORES := 6,7
+
+## CPUsets configuration
+# CPUset used for bg/audio cpusets 
+AXION_CPU_BG := 0-2
+
+# CPUset used for foreground cpusets
+AXION_CPU_FG := 2-7
+
+# CPUset that will be used when limiting other cpusets except top-app
+AXION_CPU_LIMIT_BG := 0-1
+
+# CPUset that will be used to unlimit critical cpusets for UI
+AXION_CPU_UNLIMIT_UI := 0-7
+
+# CPUset that will be used when limiting critical cpusets for UI
+AXION_CPU_LIMIT_UI := 2-5
+
+# CPUset that will be used for critical display processes
+AXION_CPU_DISPLAY := 6-7
+
+# CPUset that will be used for audio processes e.g. audioserver
+AXION_CPU_AUDIO := 0-3
