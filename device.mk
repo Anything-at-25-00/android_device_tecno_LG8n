156d06392a2553e7f5f883f5c191cb8cb94daf33
Shirayuki39 <lorddemecrius83@proton.me>
2025-08-20 10:32:14 +0900

    LG8n: Setup CustomOTA
    * Allows unofficial builds to recieve OTA by redirecting OTA fetch location to our custom ota repo
    
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- device.mk
+++ device.mk
@@ -19,7 +19,8 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
 	FrameworksResOverlayLG8n \
     SettingsProviderOverlayLG8n \
-    SystemUIOverlayLG8n
+    SystemUIOverlayLG8n \
+    UpdaterOverlayLG8n
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 31
